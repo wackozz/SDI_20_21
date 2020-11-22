f90e58436c1e31b6a0b26cae2e434754ac4fcfbc
wackoz <francescovacc4@gmail.com>
2020-11-22 15:33:35 +0100

    test modelSim

---
+++ modelSim/test_proj/test.vhd
@@ -0,0 +1,15 @@
+library ieee ;
+    use ieee.std_logic_1164.all ;
+    use ieee.numeric_std.all ;
+
+entity butterfly is
+  port (
+    clock
+  ) ;
+end butterfly ; 
+
+architecture myArch of butterfly is
+
+begin
+
+end architecture ;
