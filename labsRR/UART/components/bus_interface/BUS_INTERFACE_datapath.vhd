d31240244f6b15af4684cecf8cb27aeed8a20f3a
FEDERICA <bongofederica@libero.it>
2020-12-09 16:00:43 +0100

    DATAPATH

--- labsRR/UART/components/bus_interface/BUS_INTERFACE_datapath.vhd
+++ labsRR/UART/components/bus_interface/BUS_INTERFACE_datapath.vhd
@@ -5,8 +5,8 @@ ENTITY bus_interfaceDP IS
 PORT(Dout,TX_out: OUT STD_LOGIC_VECTOR (7 DOWNTO 0);
 RX_out,Din:IN STD_LOGIC_VECTOR (7 DOWNTO 0);
 ADD: IN STD_LOGIC_VECTOR (2 DOWNTO 0);
-R/Wn,CS,ATNack : IN STD_LOGIC;
-ATN: OUT STD_LOGIC;
+R_Wn,CS,ATNack,RESET,CLOCK : IN STD_LOGIC;
+ATN: OUT STD_LOGIC);
 END bus_interfaceDP;
 
 
@@ -26,34 +26,34 @@ END component;
 
 component  mux_2_to_1 IS
 
-PORT ( x1, y1, s : IN std_logic;
-
-m_out :OUT std_logic); 
+PORT ( x1, y1 : IN std_logic_vector(7 downto 0);
+        s:IN std_logic;
+m_out :OUT std_logic_vector(7 downto 0)); 
 END component;
 
 
 component ControlUnit_bus IS 
-PORT (RESET,CLOCK,CS_cu,R/Wn_cu: IN STD_LOGIC;  
+PORT (RESET,CLOCK,CS_cu,R_Wn_cu,ATNack_cu: IN STD_LOGIC; 
+                       
                               ADD_cu: IN STD_LOGIC_VECTOR (2 DOWNTO 0); 
-                               SEL_mux,ATN_cu: OUT STD_LOGIC                               
-                              RX_DATA,TX_DATA : OUT STD_LOGIC_VECTOR (7 DOWNTO 0); 
-                              RX_FULL,TX_EMPTY,CLRatn,ERROR,TX_EN,RX_EN: OUT STD_LOGIC_VECTOR(7 DOWNTO 0)); 
+                               SEL_mux,ATN_cu,EN_regSTATUS,EN_regCTRL,EN_regDATARX,EN_regDATATX: OUT STD_LOGIC;                             
+                              TX_DATA : OUT STD_LOGIC_VECTOR (7 DOWNTO 0); 
+                              RX_DATA : IN STD_LOGIC_VECTOR (7 DOWNTO 0);
+                              RX_FULL,TX_EMPTY,ERROR: IN STD_LOGIC_VECTOR(7 DOWNTO 0); 
+                              CLRatn,TX_EN,RX_EN: OUT STD_LOGIC_VECTOR(7 DOWNTO 0));
 END component;
 
 
-SIGNAL 	SEL_mux_s,ENABLE_regCTRL,ENABLE_regSTATUS,ENABLE_regTX,ENABLE_regRX,
-SIGNAL  Q_out_s3,Q_out_s4,
-SIGNAL  
-SIGNAL 
+SIGNAL 	SEL_mux_s,ENABLE_regCTRL,ENABLE_regSTATUS,ENABLE_regTX,ENABLE_regRX :STD_LOGIC;
+SIGNAL  Q_out_s3,Q_out_s4 :STD_LOGIC_vector (7 downto 0);
 
+
+BEGIN 
 
 SEL_mux_s<= NOT(ADD(0))AND(ADD(1));
-ENABLE_regCTRL<= ((ADD(0) AND ADD(1))AND (CS_S) AND (NOT(R/Wn)));
-ENABLE_regSTATUS <= (R/Wn) AND (CS);
-ENABLE_regTX <= ((NOT(ADD(0)) AND NOT(ADD(1))) AND (CS) AND (NOT(R/Wn)));
-ENABLE_regRX <=(R/Wn) AND (CS);
 
-BEGIN
+
+
 
 mux:mux_2_to_1 port map (x1 => Q_out_s3, y1=> Q_out_s4, s=> SEL_mux_s, m_out => Dout);
 
@@ -62,9 +62,9 @@ reg_CTRL :bus_reg port map (R=> Din, Q=>TX_out,RESET=> RESET,CLOCK=>CLOCK, Enabl
 reg_Rx_Data: bus_reg port map (R=>  RX_out , Q=>Q_out_s3, RESET=> RESET,CLOCK=>CLOCK, Enable=>ENABLE_regRX);
 reg_STATUS : bus_reg port map (R=> RX_out,Q=>Q_out_s4,RESET=> RESET,CLOCK=>CLOCK, Enable=>ENABLE_regSTATUS);
 
-control: ControlUnit_bus port map (ADD_cu => ADD,CS_cu => CS, R/Wn_cu=> R/Wn,SEL_mux => SEL_mux_s, RESET=> RESET,CLOCK=>CLOCK
+control: ControlUnit_bus port map (ADD_cu => ADD,CS_cu => CS, R_Wn_cu=> R_Wn,SEL_mux => SEL_mux_s, RESET=> RESET,CLOCK=>CLOCK,
 						            ATN_cu=> ATN, RX_FULL=> RX_out, TX_EMPTY =>RX_out, ERROR=> RX_out,CLRatn=>TX_out,TX_EN=>TX_out,RX_EN=>TX_out,
-									 RX_DATA=> RX_out,TX_DATA=>TX_out);
-	
+									 RX_DATA=> RX_out,TX_DATA=>TX_out,ATNack_cu=> ATNack, EN_regCTRL=>ENABLE_regCTRL, EN_regDATARX=> ENABLE_regRX, 
+	                              EN_regSTATUS=>ENABLE_regSTATUS, EN_regDATATX=> ENABLE_regTX);
 
 END behav;	
