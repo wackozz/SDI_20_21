354ad998068d04c566809f8ed2fcfaf2bcff3085
wackoz <francescovacc4@gmail.com>
2020-12-09 21:27:49 +0100

    transmitter DONE

--- labsRR/UART/components/tx/tx_dp.vhd
+++ labsRR/UART/components/tx/tx_dp.vhd
@@ -26,15 +26,19 @@ use ieee.numeric_std.all;
 entity tx_dp is
 
   port (
-    clock : in std_logic;
-    reset : in std_logic;
+    clock      : in  std_logic;
+    reset      : in  std_logic;
     force_one  : in  std_logic;
     force_zero : in  std_logic;
     --counter
     count_en   : in  std_logic;
+    tx_empty   : out std_logic;
     term_count : out std_logic;
+    --shift r
     p_in       : in  std_logic_vector(7 downto 0);  --input (SR)
-    TxD        : out std_logic                     --output
+    ld_en      : in  std_logic;                     -- paraellel load shift reg
+    sh_en      : in  std_logic;                     -- shift reg shift en
+    TxD        : out std_logic                      --output
     );
 
 end entity tx_dp;
@@ -43,15 +47,16 @@ architecture arch of tx_dp is
   --signal declaration
 
   --counter
-  constant N : integer := 3;
-  signal ld : std_logic;
-  signal d  : std_logic_vector(N-1 downto 0);
-  signal q  : std_logic_vector(N-1 downto 0);
+  constant N            : integer := 3;
+  signal ld             : std_logic;  --parallel load for counter                   
+  signal term_count_bus : std_logic;
+  signal d              : std_logic_vector(N-1 downto 0);
+  signal q_txempty      : std_logic_vector(N-1 downto 0);
+  signal q_c_shift      : std_logic_vector(N-1 downto 0);
   --register
-  signal ld_en : std_logic;
-  signal s_in  : std_logic;
-  signal s_out : std_logic;
-  signal p_out : std_logic_vector(7 downto 0);
+  signal s_in           : std_logic;
+  signal s_out          : std_logic;
+  signal p_out          : std_logic_vector(7 downto 0);
 
 
   -- component inst
@@ -72,6 +77,7 @@ architecture arch of tx_dp is
     port (
       clock : in  std_logic;
       ld_en : in  std_logic;
+      sh_en : in  std_logic;
       s_in  : in  std_logic;
       s_out : out std_logic;
       p_in  : in  std_logic_vector(7 downto 0);
@@ -86,6 +92,7 @@ begin  -- architecture arch
     port map (
       clock => clock,
       ld_en => ld_en,
+      sh_en => sh_en,
       s_in  => s_in,
       s_out => s_out,
       p_in  => p_in,
@@ -93,7 +100,7 @@ begin  -- architecture arch
       );
   -- port map: counter
 
-  counter : counter_nbit
+  counter_shift : counter_nbit
     generic map (N => N)
     port map (
       clock => clock,
@@ -101,22 +108,44 @@ begin  -- architecture arch
       en    => count_en,
       ld    => ld,
       d     => d,
-      q     => q
+      q     => q_c_shift
       );
 
-  terminal_counter: process (clock) is
+  counter_txempty : counter_nbit
+    generic map (N => N)
+    port map (
+      clock => clock,
+      clear => reset,
+      en    => term_count_bus,
+      ld    => ld,
+      d     => d,
+      q     => q_txempty
+      );
+
+
+  terminal_counter : process (clock, reset) is
   begin  -- process terminal_counter
-    
-    if clock'event and clock = '1' then  -- rising clock edge
-      if unsigned(q) = 7 then
+
+    if reset = '1' then
+      tx_empty   <= '1';  -- important to set tx_empty to 1 during idle stage
+      term_count <= '0';
+    elsif clock'event and clock = '1' then  -- rising clock edge
+
+      if(unsigned(q_c_shift) = 139) then
         term_count <= '1';
       else
         term_count <= '0';
-        end if;
+      end if;
+
+      if unsigned(q_txempty) = 7 then
+        tx_empty <= '1';
+      else
+        tx_empty <= '0';
+      end if;
     end if;
   end process terminal_counter;
 
-  TxD <= ((s_out and not(force_zero)) and force_one);
-
+  TxD        <= ((s_out and not(force_zero)) and force_one);
+  term_count <= term_count_bus;
 end architecture arch;
 
