ac77f59864e308eb8ad53a930733cde0bca75911
wackoz <francescovacc4@gmail.com>
2021-01-05 17:02:33 +0100

    merge

--- butterfly/vhd/quartus control/controlunit_butterfly_tb.vhd
+++ butterfly/vhd/quartus control/controlunit_butterfly_tb.vhd
@@ -1,3 +1,24 @@
+-------------------------------------------------------------------------------
+-- Title      : Testbench for design "controlunit_butterfly"
+-- Project    : 
+-------------------------------------------------------------------------------
+-- File       : controlunit_butterfly_tb.vhd
+-- Author     : wackoz  <wackoz@wT14s>
+-- Company    : 
+-- Created    : 2021-01-04
+-- Last update: 2021-01-04
+-- Platform   : 
+-- Standard   : VHDL'93/02
+-------------------------------------------------------------------------------
+-- Description: 
+-------------------------------------------------------------------------------
+-- Copyright (c) 2021 
+-------------------------------------------------------------------------------
+-- Revisions  :
+-- Date        Version  Author  Description
+-- 2021-01-04  1.0      wackoz	Created
+-------------------------------------------------------------------------------
+
 library ieee;
 use ieee.std_logic_1164.all;
 
@@ -11,45 +32,45 @@ end entity controlunit_butterfly_tb;
 
 architecture arch of controlunit_butterfly_tb is
 
-component controlunit_butterfly is
-
-port ( status: in std_logic_vector ( 1 downto 0);
-       clock: in std_logic;
-		 datapath_commands : out std_logic_vector ( 33 downto 0));
-		 end component;
-
   -- component ports
-  signal datapath_commands                           : std_logic_vector (33 downto 0);
-  signal status                         : std_logic_vector (1 downto 0);
+  signal status            : std_logic_vector (1 downto 0);
+  signal clock             : std_logic;
+  signal reset             : std_logic;
+  signal datapath_commands : std_logic_vector (18 downto 0);
 
   -- clock
-  signal clock : std_logic := '1';
+  signal Clk : std_logic := '1';
 
 begin  -- architecture arch
 
   -- component instantiation
-  DUT : controlunit_butterfly
+  DUT: entity work.controlunit_butterfly
     port map (
-       datapath_commands => datapath_commands,
-		 status => status,
-		 clock => clock
-     );
+      status            => status,
+      clock             => clock,
+      reset             => reset,
+      datapath_commands => datapath_commands);
 
   -- clock generation
-  clock <= not clock after 10 ns;
+  Clk <= not Clk after 10 ns;
 
   -- waveform generation
-  WaveGen_Proc : process
+  WaveGen_Proc: process
   begin
     -- insert signal assignments here
-status <= "00";
-wait for 50 ns;
-status <= "00";
-wait for 50 ns;
-status <= "10";
-wait;
+
+    wait until Clk = '1';
   end process WaveGen_Proc;
 
-
+  
 
 end architecture arch;
+
+-------------------------------------------------------------------------------
+
+configuration controlunit_butterfly_tb_arch_cfg of controlunit_butterfly_tb is
+  for arch
+  end for;
+end controlunit_butterfly_tb_arch_cfg;
+
+-------------------------------------------------------------------------------
--- butterfly/vhd/quartus control/pla_status.vhd
+++ butterfly/vhd/quartus control/pla_status.vhd
@@ -2,41 +2,41 @@ library ieee;
 use ieee.std_logic_1164.all;
 
 entity PLA_status is
-port (full_speed,start,lsb_in: in std_logic;
-      cc_validation : in std_logic_vector(1 downto 0);
-		s_mux_late,LSB_out : out std_logic);
+  port (full_speed, start, lsb_in : in  std_logic;
+        cc_validation             : in  std_logic_vector(1 downto 0);
+        s_mux_late, LSB_out       : out std_logic);
 end PLA_status;
 
 
 architecture behav of PLA_status is
 
-component mux_2_to_1 IS
-port( x1,y1 : in std_logic;
-      s: in std_logic;
-		m1: out std_logic);
+  component mux_2_to_1 is
+    port(x1, y1 : in  std_logic;
+         s      : in  std_logic;
+         m1     : out std_logic);
 
-end component;
+  end component;
 
-signal full_speed_s,start_s,s_mux_int : std_logic;
-signal cc_validation_s1,cc_validation_s2 : std_logic;
-signal lsb_in_s :std_logic;
+  signal full_speed_s, start_s, s_mux_int   : std_logic;
+  signal cc_validation_s1, cc_validation_s2 : std_logic;
+  signal lsb_in_s                           : std_logic;
 
 begin
-cc_validation_s1 <= (cc_validation(1) AND (NOT(cc_validation(0))));
-cc_validation_s2 <= (cc_validation(1) AND (cc_validation(0)));
+  cc_validation_s1 <= (cc_validation(1) and (not(cc_validation(0))));
+  cc_validation_s2 <= (cc_validation(1) and (cc_validation(0)));
 
-full_speed_s <= (cc_validation_s2 AND full_speed);
-start_s <= (cc_validation_s1 AND start);
+  full_speed_s <= (cc_validation_s2 and full_speed);
+  start_s      <= (cc_validation_s1 and start);
 
 --mux interno al PLA
-s_mux_int <= full_speed_s OR start_s;
+  s_mux_int <= full_speed_s or start_s;
 
 --selettore del mux esterno
-s_mux_late <= (NOT cc_validation(0)) AND (NOT(cc_validation(1)));
+  s_mux_late <= (not cc_validation(0)) and (not(cc_validation(1)));
 
-lsb_in_s <= not lsb_in;
+  lsb_in_s <= not lsb_in;
 
-mux:
-mux_2_to_1 port map (x1 => lsb_in, y1 => lsb_in_s, s => s_mux_int, m1 => LSB_out); 
+  mux :
+    mux_2_to_1 port map (x1 => lsb_in, y1 => lsb_in_s, s => s_mux_int, m1 => LSB_out);
 
 end behav;
